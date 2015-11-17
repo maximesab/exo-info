4b103708f092dc9482cd794f5e31a020d19a52a7
gsabran <sabranguillaume@gmail.com>
2015-11-17 15:31:11 -0800

    use binary operations and logic to solve binary questions

--- exo info.py
+++ exo info.py
@@ -19,15 +19,28 @@ def fibo(n):
         
 #ex 5.1
  
+def to_bin(n):
+    return ''.join(str(1 & int(n) >> i) for i in range(64)[::-1])
+      
+def bitnumbers_debug (N,M,i,j):
+    mask = ~0 # 111111 32 times
+    print to_bin(mask)
+    mask = mask << j-i+1 # 1111110000     1 32 - j-1+i times, 0 j-i+1 times
+    print to_bin(mask)
+    mask = ~ mask # 0000001111      0 32 - j-1+i times, 1 j-i+1 times
+    print to_bin(mask)
+    mask = mask << i # 0000001111000 0     32 - j-1 times, 1 j-i+1 times, 0 i times
+    print to_bin(mask)
+    print to_bin((N & ~mask) | (M << i))
+    return (N & ~mask) | (M << i)
       
 def bitnumbers (N,M,i,j):
-    
-    N1 = N - decimalpart(N*math.pow(10,-j))*math.pow(10,j)
-    
-    N2 = N - decimalpart(N*math.pow(10,-i))*math.pow(10,i)
-    
-    N3 = N +N1 -N2
-    return N3 + M*math.pow(10,i)
+    mask = ~0 # 111111 32 times
+    mask = mask << j-i+1 # 1111110000     1 32 - j-1+i times, 0 j-i+1 times
+    mask = ~ mask # 0000001111      0 32 - j-1+i times, 1 j-i+1 times
+    mask = mask << i # 0000001111000 0     32 - j-1 times, 1 j-i+1 times, 0 i times
+    return (N & ~mask) | (M << i)
+
         
 
 def decimalpart(N):
